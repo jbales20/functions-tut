6ff67d02d880abaf41900d5f95d32e9733b14a8a
jbales20 <69781414+jbales20@users.noreply.github.com>
2020-09-08 23:46:51 -0400

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,1 +1,25 @@
-# functions-tut
+def area(width, height):
+    result = width * height
+    return result
+
+def subtract(num1, num2):
+    result = num1 - num2
+    return result
+
+# The add function takes the augend and the addend and adds them to find the sum. 
+def add(num1, num2):
+    result = num1 + num2
+    return result
+
+
+result = area(5, 6)
+# print(result)
+
+area1 = area(2, 4)
+area2 = area(12, 12)
+
+subtract1 = subtract(10, 5)
+subtract2 = subtract(120, 60)
+
+add1 = add(2, 2)
+add2 = add(24, 24)
